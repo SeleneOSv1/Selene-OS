10851935706ee87d3f79434198cb281ecb6a76d4
john <john@selene.inc>
2026-04-23 20:45:06 +0800

    style: lighten the desktop sidebar tint

--- apple/mac_desktop/SeleneMacDesktop/DesktopSessionShellView.swift
+++ apple/mac_desktop/SeleneMacDesktop/DesktopSessionShellView.swift
@@ -4890,9 +4890,9 @@ struct DesktopSessionShellView: View {
         let historySurfaces = desktopSidebarHistorySurfaces
         let sidebarBackground = Color(
             nsColor: NSColor(
-                srgbRed: 0.968,
-                green: 0.963,
-                blue: 0.955,
+                srgbRed: 0.974,
+                green: 0.970,
+                blue: 0.963,
                 alpha: 1.0
             )
         )
